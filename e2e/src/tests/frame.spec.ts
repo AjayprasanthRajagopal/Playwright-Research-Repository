d86744fb6c472fbf3584b9ed8ce37f3a25d6a04f
Ajay <itzmeajay@yahoo.com>
2023-11-10 03:55:45 +0530

    Update frame.spec.ts

--- e2e/src/tests/frame.spec.ts
+++ e2e/src/tests/frame.spec.ts
@@ -19,9 +19,9 @@ test.describe("", async () => {
     await test.step("Click Button tab", async () => {
       await letCodeFramePage.clickFrameTab();
       //Handle single frame
-      await letCodeFramePage.firstName().fill(testData.letCode.frame.firstName, { timeout:2000 });
-      await letCodeFramePage.lastName().fill(testData.letCode.frame.lastName, { timeout:2000 });
-      await letCodeFramePage.email().fill(testData.letCode.frame.email, { timeout:2000 });
+      await letCodeFramePage.firstName().fill(testData.letCode.frame.firstName, { timeout:20000 });
+      await letCodeFramePage.lastName().fill(testData.letCode.frame.lastName, { timeout:20000 });
+      await letCodeFramePage.email().fill(testData.letCode.frame.email, { timeout:20000 });
     });
 
 
